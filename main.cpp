fad92802a5415390197d41eb92d194160034a53f
mastershare <mastershare92@gmail.com>
2015-03-22 01:36:46 +0100

    Promenjene adrese, svedimo da nam budu iste.
    Podesite Repository i klon projekta na  C:/FenixTanks

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow window;
 
-    QIcon icon("C:/Users/lenovo/Desktop/FenixTanks/img/tank_icon.png");
+    QIcon icon("C:/FenixTanks/img/tank_icon.png");
     window.setWindowIcon(icon);
 
     window.showMaximized();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,11 +8,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //moguce je da ce te morati da promenite putanju da odgovara vama
-    //moja je: C:/Users/lenovo/Desktop/FenixTanks/img/bg.png
+    //Podesiti da GitHub sinhronizuje projekat na sl. adresi C:\FenixTanks
+    //Posto svi imamo C
 
     m_pPalette	= new QPalette();
-    m_pPixmap		= new QPixmap("C:/Users/lenovo/Desktop/FenixTanks/img/bg.png");
+    m_pPixmap	= new QPixmap("C:/FenixTanks/img/bg.png");
 
     m_pPalette->setBrush(QPalette::Background,QBrush(*m_pPixmap));
     setPalette(*m_pPalette);
