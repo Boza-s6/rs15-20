4b8fdbabd79752b5ba2972b23990dc147e6a8104
Milan <mastershare92@gmail.com>
2015-04-01 16:01:59 +0200

    Dodate su ikonice na btn exit, 1player i 2players.
    Promenjen je i naziv ikonica u img fajlu, radi iste notacije.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,7 +15,21 @@ MainWindow::MainWindow(QWidget *parent) :
     m_pPalette->setBrush(QPalette::Background,QBrush(*m_pPixmap));
     setPalette(*m_pPalette);
 
+    //Exit, 1 Player, 2 Players btn img
+    QIcon exit;
+    QIcon player;
+    QIcon players;
+    exit.addFile(QString::fromUtf8("img/exit.jpg"), QSize(), QIcon::Normal, QIcon::Off);
+    player.addFile(QString::fromUtf8("img/player1.jpg"), QSize(), QIcon::Normal, QIcon::Off);
+    players.addFile(QString::fromUtf8("img/player2.jpg"), QSize(), QIcon::Normal, QIcon::Off);
+    ui->btn_exit->setIcon(exit);
+    ui->btn_1player->setIcon(player);
+    ui->btn_2players->setIcon(players);
+    ui->btn_exit->setIconSize(QSize(156, 52));
+    ui->btn_1player->setIconSize(QSize(156, 52));
+    ui->btn_2players->setIconSize(QSize(156, 52));
 }
+
 //kad se prozor resizuje da se scalira slika
 void MainWindow::resizeEvent (QResizeEvent* event)
 {
